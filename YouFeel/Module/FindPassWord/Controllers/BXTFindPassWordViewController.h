47dd7f0acc48b45aaf359563229547a6a663f37b
Jason318 <834699083@qq.com>
2015-11-02 09:46:27 +0800

    c

---
+++ YouFeel/Module/FindPassWord/Controllers/BXTFindPassWordViewController.h
@@ -0,0 +1,13 @@
+//
+//  BXTFindPassWordViewController.h
+//  YouFeel
+//
+//  Created by Jason on 15/10/30.
+//  Copyright © 2015年 Jason. All rights reserved.
+//
+
+#import "BXTBaseViewController.h"
+
+@interface BXTFindPassWordViewController : BXTBaseViewController
+
+@end
---
+++ YouFeel/Module/FindPassWord/Controllers/BXTFindPassWordViewController.m
@@ -0,0 +1,287 @@
+//
+//  BXTFindPassWordViewController.m
+//  YouFeel
+//
+//  Created by Jason on 15/10/30.
+//  Copyright © 2015年 Jason. All rights reserved.
+//
+
+#import "BXTFindPassWordViewController.h"
+#import "BXTHeaderForVC.h"
+#import "BXTResignTableViewCell.h"
+#import "BXTChangePassWordViewController.h"
+
+#define UserNameTag 11
+#define CodeTag 12
+
+@interface BXTFindPassWordViewController ()<UITableViewDataSource,UITableViewDelegate,UITextFieldDelegate,BXTDataResponseDelegate>
+{
+    UIButton *codeBtn;
+    NSString *userName;
+    NSString *codeNumber;
+}
+@end
+
+@implementation BXTFindPassWordViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self navigationSetting:@"找回密码" andRightTitle:nil andRightImage:nil];
+    [self initContentViews];
+}
+
+#pragma mark -
+#pragma mark 初始化视图
+- (void)initContentViews
+{
+    UITableView *currentTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, KNAVIVIEWHEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT - KNAVIVIEWHEIGHT) style:UITableViewStyleGrouped];
+    [currentTableView registerClass:[BXTResignTableViewCell class] forCellReuseIdentifier:@"Cell"];
+    currentTableView.delegate = self;
+    currentTableView.dataSource = self;
+    [self.view addSubview:currentTableView];
+}
+
+#pragma mark -
+#pragma mark 事件处理
+- (void)nextTapClick
+{
+    if ([BXTGlobal validateMobile:userName])
+    {
+        BXTDataRequest *request = [[BXTDataRequest alloc] initWithDelegate:self];
+        [request findPassWordWithMobile:userName andWithCode:codeNumber];
+    }
+    else
+    {
+        [self showMBP:@"手机号格式不对" withBlock:nil];
+    }
+}
+
+- (void)getVerCode
+{
+    if ([BXTGlobal validateMobile:userName])
+    {
+        BXTDataRequest *request = [[BXTDataRequest alloc] initWithDelegate:self];
+        [request mobileVerCode:userName];
+        codeBtn.userInteractionEnabled = NO;
+        [codeBtn setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+    }
+    else
+    {
+        [self showMBP:@"手机号格式不对" withBlock:nil];
+    }
+}
+
+#pragma mark -
+#pragma mark 代理
+/**
+ *  UITextFiledDelegate
+ */
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+    NSString *resultString = [textField.text stringByReplacingCharactersInRange:range withString:string];
+    if (textField.tag == UserNameTag)
+    {
+        userName = resultString;
+        if (resultString.length > 11)
+        {
+            return NO;
+        }
+    }
+    else if (textField.tag == CodeTag)
+    {
+        codeNumber = resultString;
+    }
+    return YES;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return YES;
+}
+
+/**
+ *  UITableViewDelegate & UITableViewDatasource
+ */
+//section头部间距
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    if (section == 0)
+    {
+        return 16.f;//section头部高度
+    }
+    return 10.f;//section头部高度
+}
+//section头部视图
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    UIView *view;
+    if (section == 0)
+    {
+        view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 16.f)];
+    }
+    view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 10.f)];
+    view.backgroundColor = [UIColor clearColor];
+    return view;
+}
+//section底部间距
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    if (section == 1)
+    {
+        return 80.f;
+    }
+    return 5.f;
+}
+//section底部视图
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section
+{
+    if (section == 1)
+    {
+        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 80.f)];
+        view.backgroundColor = [UIColor clearColor];
+        UIButton *nextTapBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        nextTapBtn.frame = CGRectMake(20, 30, SCREEN_WIDTH - 40, 50.f);
+        [nextTapBtn setTitle:@"提交" forState:UIControlStateNormal];
+        [nextTapBtn setTitleColor:colorWithHexString(@"ffffff") forState:UIControlStateNormal];
+        [nextTapBtn setBackgroundColor:colorWithHexString(@"aac3e1")];
+        nextTapBtn.layer.masksToBounds = YES;
+        nextTapBtn.layer.cornerRadius = 6.f;
+        [nextTapBtn addTarget:self action:@selector(nextTapClick) forControlEvents:UIControlEventTouchUpInside];
+        [view addSubview:nextTapBtn];
+        return view;
+    }
+    else
+    {
+        UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 5.f)];
+        view.backgroundColor = [UIColor clearColor];
+        return view;
+    }
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 50.f;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    BXTResignTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    
+    if (indexPath.section == 0)
+    {
+        cell.nameLabel.text = @"手机号";
+        cell.textField.placeholder = @"请输入有效的手机号码";
+        cell.textField.keyboardType = UIKeyboardTypeNumberPad;
+        cell.textField.tag = UserNameTag;
+        cell.codeButton.hidden = YES;
+    }
+    else
+    {
+        cell.nameLabel.text = @"验证码";
+        cell.textField.placeholder = @"请输入短信验证码";
+        cell.textField.keyboardType = UIKeyboardTypeNumberPad;
+        cell.codeButton.hidden = NO;
+        [cell.codeButton addTarget:self action:@selector(getVerCode) forControlEvents:UIControlEventTouchUpInside];
+        codeBtn = cell.codeButton;
+        cell.textField.tag = CodeTag;
+    }
+    
+    cell.boyBtn.hidden = YES;
+    cell.girlBtn.hidden = YES;
+    cell.textField.delegate = self;
+    
+    return cell;
+}
+
+/**
+ *  BXTDataRequestDelegate
+ */
+- (void)requestResponseData:(id)response requeseType:(RequestType)type
+{
+    NSDictionary *dic = response;
+    if ([[dic objectForKey:@"returncode"] integerValue] == 0)
+    {
+        if (type == FindPassword)
+        {
+            NSArray *data = [dic objectForKey:@"data"];
+            NSDictionary *dictionary = data[0];
+            [BXTGlobal setUserProperty:userName withKey:U_USERNAME];
+            BXTChangePassWordViewController *changeVC = [[BXTChangePassWordViewController alloc] initWithID:[dictionary objectForKey:@"id"] andWithKey:[dictionary objectForKey:@"key"]];
+            [self.navigationController pushViewController:changeVC animated:YES];
+        }
+        else
+        {
+            [self updateTime];
+        }
+    }
+    else
+    {
+        if (type == FindPassword)
+        {
+            [self showMBP:@"验证码输入有误！" withBlock:nil];
+        }
+    }
+}
+
+- (void)updateTime
+{
+    __block NSInteger count = 60;
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_source_t _time = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
+    dispatch_source_set_timer(_time, dispatch_walltime(NULL, 3), 1.0 * NSEC_PER_SEC, 0);
+    dispatch_source_set_event_handler(_time, ^{
+        count--;
+        if (count <= 0)
+        {
+            dispatch_source_cancel(_time);
+            dispatch_async(dispatch_get_main_queue(), ^{
+                codeBtn.userInteractionEnabled = YES;
+                [codeBtn setTitleColor:colorWithHexString(@"3cafff") forState:UIControlStateNormal];
+                [codeBtn setTitle:@"获取验证码" forState:UIControlStateNormal];
+            });
+        }
+        else
+        {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [codeBtn setTitle:[NSString stringWithFormat:@"重新获取 %ld",(long)count] forState:UIControlStateNormal];
+            });
+        }
+    });
+    dispatch_resume(_time);
+}
+
+- (void)requestError:(NSError *)error
+{
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
