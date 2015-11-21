660d6bdb64e7ba3b0d85de1a5c36455ec95e2604
ManXiaoYi <994525441@qq.com>
2015-11-21 17:45:08 +0800

    抢单

--- YouFeel/Module/GrabOrder/Views/RGCollectionViewCell.h
+++ YouFeel/Module/GrabOrder/Views/RGCollectionViewCell.h
@@ -11,12 +11,16 @@
 
 @interface RGCollectionViewCell : UICollectionViewCell<BXTDataResponseDelegate>
 
+@property (strong, nonatomic) UIScrollView     *scrollView;
 @property (strong, nonatomic) UIImageView      *imageView;
+@property (strong, nonatomic) UILabel          *repairTime;
+@property (strong, nonatomic) UILabel          *faulttype;
 @property (strong, nonatomic) UILabel          *repairID;
 @property (strong, nonatomic) UILabel          *location;
 @property (strong, nonatomic) UILabel          *cause;
 @property (strong, nonatomic) UILabel          *notes;
 @property (strong, nonatomic) UILabel          *level;
+@property (strong, nonatomic) UILabel          *remarks;
 //@property (strong, nonatomic) UIView           *backView;
 //@property (strong, nonatomic) UILabel          *coinLabel;
 
--- YouFeel/Module/GrabOrder/Views/RGCollectionViewCell.m
+++ YouFeel/Module/GrabOrder/Views/RGCollectionViewCell.m
@@ -19,19 +19,53 @@
     if (self)
     {
         self.backgroundColor = colorWithHexString(@"ffffff");
-        self.layer.cornerRadius = 10.f;
-        self.layer.masksToBounds = YES;
+        //self.layer.cornerRadius = 10.f;
+        //self.layer.masksToBounds = YES;
 
         CGFloat space = IS_IPHONE6 ? 15.f : 10.f;
-        CGFloat height = IS_IPHONE6 ? 200.f : 133;
+        CGFloat height = IS_IPHONE6 ? 263.f : 175;
+        CGFloat moreH = IS_IPHONE6 ? 80.f : 70;
         
+        self.scrollView = ({
+            
+            UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, frame.size.height-moreH)];
+            [self addSubview:scrollView];
+            scrollView;
+            
+        });
+
         self.imageView = ({
         
             UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(space, space, frame.size.width - space * 2.f, height)];
             imgView.contentMode = UIViewContentModeScaleAspectFill;
             imgView.layer.masksToBounds = YES;
-            [self addSubview:imgView];
+            [self.scrollView addSubview:imgView];
             imgView;
+            
+        });
+        
+        self.repairTime = ({
+            
+            UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(_imageView.frame) - 40.f, 165, 25)];
+            label.backgroundColor = colorWithHexString(@"c30e06");
+            label.textColor = colorWithHexString(@"ffffff");
+            label.font = [UIFont boldSystemFontOfSize:15.f];
+            [self.imageView addSubview:label];
+            label;
+            
+        });
+        
+        self.faulttype = ({
+            
+            UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(frame.size.width-15-80, CGRectGetMaxY(_imageView.frame) + 10.f, 60, 25)];
+            label.textColor = colorWithHexString(@"3cb7ff");
+            label.font = [UIFont boldSystemFontOfSize:16.f];
+            label.textAlignment = NSTextAlignmentCenter;
+            label.layer.borderColor = [colorWithHexString(@"3cb7ff") CGColor];
+            label.layer.borderWidth = 1.f;
+            label.layer.cornerRadius = 5.f;
+            [self.scrollView addSubview:label];
+            label;
             
         });
         
@@ -40,7 +74,7 @@
             UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(15.f, CGRectGetMaxY(_imageView.frame) + 15.f, CGRectGetWidth(_imageView.frame), 20)];
             label.textColor = colorWithHexString(@"000000");
             label.font = [UIFont boldSystemFontOfSize:17.f];
-            [self addSubview:label];
+            [self.scrollView addSubview:label];
             label;
         
         });
@@ -50,7 +84,7 @@
             UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(15.f, CGRectGetMaxY(_repairID.frame) + 10.f, CGRectGetWidth(_repairID.frame), 20)];
             label.textColor = colorWithHexString(@"000000");
             label.font = [UIFont boldSystemFontOfSize:17.f];
-            [self addSubview:label];
+            [self.scrollView addSubview:label];
             label;
         
         });
@@ -60,7 +94,7 @@
             UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(15.f, CGRectGetMaxY(_location.frame) + 10.f, CGRectGetWidth(_location.frame), 20)];
             label.textColor = colorWithHexString(@"000000");
             label.font = [UIFont boldSystemFontOfSize:17.f];
-            [self addSubview:label];
+            [self.scrollView addSubview:label];
             label;
             
         });
@@ -72,7 +106,7 @@
             label.numberOfLines = 0;
             label.lineBreakMode = NSLineBreakByTruncatingTail;
             label.font = [UIFont boldSystemFontOfSize:17.f];
-            [self addSubview:label];
+            [self.scrollView addSubview:label];
             label;
         
         });
@@ -82,9 +116,21 @@
             UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(15.f, CGRectGetMaxY(_notes.frame) + 10.f, CGRectGetWidth(_notes.frame), 20)];
             label.textColor = colorWithHexString(@"000000");
             label.font = [UIFont boldSystemFontOfSize:17.f];
-            [self addSubview:label];
+            [self.scrollView addSubview:label];
             label;
         
+        });
+        
+        self.remarks = ({
+            
+            UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(15.f, CGRectGetMaxY(_level.frame) + 10.f, CGRectGetWidth(_level.frame), 20)];
+            label.textColor = colorWithHexString(@"000000");
+            label.numberOfLines = 0;
+            label.lineBreakMode = NSLineBreakByTruncatingTail;
+            label.font = [UIFont boldSystemFontOfSize:17.f];
+            [self.scrollView addSubview:label];
+            label;
+            
         });
         
 //        self.backView = ({
@@ -125,6 +171,67 @@
     /**获取详情**/
     BXTDataRequest *request = [[BXTDataRequest alloc] initWithDelegate:self];
     [request repairDetail:orderID];
+    
+    
+//    UIImage *placeImage;
+//    if (IS_IPHONE6P)
+//    {
+//        placeImage = [UIImage imageNamed:@"grabIphoneplus"];
+//    }
+//    else if (IS_IPHONE6)
+//    {
+//        placeImage = [UIImage imageNamed:@"grabIphone6"];
+//    }
+//    else if (IS_IPHONE5)
+//    {
+//        placeImage = [UIImage imageNamed:@"grabIphone5s"];
+//    }
+//    else
+//    {
+//        placeImage = [UIImage imageNamed:@"grabIphone4s"];
+//    }
+//  
+//        [_imageView sd_setImageWithURL:[NSURL URLWithString:@"http://api.51bxt.com/file/4/workorder/fault_pic/2015112116351124526.jpg"] placeholderImage:placeImage];
+// 
+//    _repairTime.text = [NSString stringWithFormat:@"工单响应截止时间%@", [self transTimeStampToTime:@"1448095067"]];
+//    
+//    
+//    NSString *contents0 = @"综修理";
+//    UIFont *font0 = [UIFont boldSystemFontOfSize:16.f];
+//    CGSize size0 = MB_MULTILINE_TEXTSIZE(contents0, font0, CGSizeMake(SCREEN_WIDTH - 40.f, 1000.f), NSLineBreakByWordWrapping);
+//    _faulttype.frame = CGRectMake(_imageView.frame.size.width-size0.width-15, CGRectGetMaxY(_imageView.frame)+10, size0.width+10, 25);
+//    _faulttype.text = contents0;
+//    
+//    _repairID.text = [NSString stringWithFormat:@"工单号：%@", @"2011121020"];
+//    
+//    _location.text = [NSString stringWithFormat:@"位置：%@",@"一层商铺"];
+//    
+//    NSString *contents = [NSString stringWithFormat:@"故障描述：%@",@"个梵蒂冈"];
+//    UIFont *font = [UIFont boldSystemFontOfSize:17.f];
+//    CGSize size = MB_MULTILINE_TEXTSIZE(contents, font, CGSizeMake(SCREEN_WIDTH - 40.f, 1000.f), NSLineBreakByWordWrapping);
+//    
+//    _cause.text = [NSString stringWithFormat:@"故障类型：%@",@"灯不亮"];
+//    _notes.frame = CGRectMake(15.f, CGRectGetMaxY(_cause.frame) + 10.f, CGRectGetWidth(_cause.frame), size.height);
+//    _level.frame = CGRectMake(15.f, CGRectGetMaxY(_notes.frame) + 10.f, CGRectGetWidth(_notes.frame), 20);
+//    _notes.text = contents;
+//    
+//    NSString *str;
+//    NSRange range;
+//
+//        str = @"等级：紧急";
+//        range = [str rangeOfString:@"紧急"];
+//
+//    NSMutableAttributedString *attributeStr = [[NSMutableAttributedString alloc] initWithString:str];
+//    [attributeStr addAttribute:NSForegroundColorAttributeName value:colorWithHexString(@"de1a1a") range:range];
+//    _level.attributedText = attributeStr;
+//    
+//    NSString *contents2 = [NSString stringWithFormat:@"备注：%@", @"备注：法规到公司的分公司的给答复是股份公司的分公司的风格是大法官"];
+//    CGSize size2 = MB_MULTILINE_TEXTSIZE(contents2, font, CGSizeMake(SCREEN_WIDTH - 40.f, 1000.f), NSLineBreakByWordWrapping);
+//    _remarks.frame = CGRectMake(15.f, CGRectGetMaxY(_level.frame) + 10.f, CGRectGetWidth(_level.frame), size2.height);
+//    _remarks.text = contents2;
+//    
+//    _scrollView.contentSize = CGSizeMake(_scrollView.bounds.size.width, CGRectGetMaxY(_remarks.frame));
+
 }
 
 #pragma mark -
@@ -173,13 +280,24 @@
             [_imageView setImage:placeImage];
         }
         
-        _repairID.text = [NSString stringWithFormat:@"工单号:%@",repairDetail.orderid];
-        _location.text = [NSString stringWithFormat:@"位置:%@",repairDetail.place_name];
-        NSString *contents = [NSString stringWithFormat:@"故障描述:%@",repairDetail.cause];
+        _repairTime.text = [NSString stringWithFormat:@"工单响应截止时间%@", [self transTimeStampToTime:repairDetail.repair_time]];
+        
+        NSString *contents0 = @"综修理";
+        UIFont *font0 = [UIFont boldSystemFontOfSize:16.f];
+        CGSize size0 = MB_MULTILINE_TEXTSIZE(contents0, font0, CGSizeMake(SCREEN_WIDTH - 40.f, 1000.f), NSLineBreakByWordWrapping);
+        _faulttype.frame = CGRectMake(_imageView.frame.size.width-size0.width-15, CGRectGetMaxY(_imageView.frame)+10, size0.width+10, 25);
+        _faulttype.text = contents0;
+        
+        _repairID.text = [NSString stringWithFormat:@"工单号：%@",repairDetail.orderid];
+        
+        _location.text = [NSString stringWithFormat:@"位置：%@",repairDetail.place_name];
+        
+        NSString *contents = [NSString stringWithFormat:@"故障描述：%@",repairDetail.cause];
         UIFont *font = [UIFont boldSystemFontOfSize:17.f];
         CGSize size = MB_MULTILINE_TEXTSIZE(contents, font, CGSizeMake(SCREEN_WIDTH - 40.f, 1000.f), NSLineBreakByWordWrapping);
-        _cause.text = [NSString stringWithFormat:@"故障类型:%@",repairDetail.faulttype_name];
-        _notes.frame = CGRectMake(15.f, CGRectGetMaxY(_cause.frame) + 10.f, CGRectGetWidth(_cause.frame), size.height > 50.f ? 50 : size.height);
+        
+        _cause.text = [NSString stringWithFormat:@"故障类型：%@",repairDetail.faulttype_name];
+        _notes.frame = CGRectMake(15.f, CGRectGetMaxY(_cause.frame) + 10.f, CGRectGetWidth(_cause.frame), size.height);
         _level.frame = CGRectMake(15.f, CGRectGetMaxY(_notes.frame) + 10.f, CGRectGetWidth(_notes.frame), 20);
         _notes.text = contents;
         
@@ -187,23 +305,38 @@
         NSRange range;
         if (repairDetail.urgent == 1)
         {
-            str = @"等级:紧急";
+            str = @"等级：紧急";
             range = [str rangeOfString:@"紧急"];
         }
         else
         {
-            str = @"等级:一般";
+            str = @"等级：一般";
             range = [str rangeOfString:@"一般"];
         }
         NSMutableAttributedString *attributeStr = [[NSMutableAttributedString alloc] initWithString:str];
         [attributeStr addAttribute:NSForegroundColorAttributeName value:colorWithHexString(@"de1a1a") range:range];
         _level.attributedText = attributeStr;
+        
+        NSString *contents2 = [NSString stringWithFormat:@"备注：%@", repairDetail.notes];
+        CGSize size2 = MB_MULTILINE_TEXTSIZE(contents2, font, CGSizeMake(SCREEN_WIDTH - 40.f, 1000.f), NSLineBreakByWordWrapping);
+        _remarks.frame = CGRectMake(15.f, CGRectGetMaxY(_level.frame) + 10.f, CGRectGetWidth(_level.frame), size2.height);
+        _remarks.text = contents2;
+        
+        _scrollView.contentSize = CGSizeMake(_scrollView.bounds.size.width, CGRectGetMaxY(_remarks.frame));
     }
 }
 
 - (void)requestError:(NSError *)error
 {
     
+}
+
+- (NSString *)transTimeStampToTime:(NSString *)timeStr
+{
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"HH:mm"];
+    NSString *dateTime = [formatter stringFromDate:[NSDate dateWithTimeIntervalSince1970:[timeStr doubleValue]]];
+    return dateTime;
 }
 
 @end
