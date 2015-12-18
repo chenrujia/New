55b2cc0518d7e31f843c0218a795c85998ca50d3
ManXiaoYi <994525441@qq.com>
2015-12-18 13:41:07 +0800

    哈哈哈

--- YouFeel/Module/Statistics/Statistics/Controllers/BXTStatisticsViewController.m
+++ YouFeel/Module/Statistics/Statistics/Controllers/BXTStatisticsViewController.m
@@ -32,6 +32,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    
     [self navigationSetting:@"业务统计" andRightTitle:nil andRightImage:[UIImage imageNamed:@"w_small_round"]];
     
     self.dataArray = [[NSMutableArray alloc] initWithObjects:@"维修完成率统计", @"专业分组统计", @"故障发生率统计", @"维修员工作量统计", @"维修评价统计",nil];
@@ -46,6 +47,7 @@
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
     [self.view addSubview:self.tableView];
+    
 }
 
 - (void)navigationRightButton
