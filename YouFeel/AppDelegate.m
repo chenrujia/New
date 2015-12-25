cf54e3dedf9c3266495d171fd7defe0465fd4c66
ManXiaoYi <994525441@qq.com>
2015-12-25 18:21:43 +0800

    commit

--- YouFeel/AppDelegate.m
+++ YouFeel/AppDelegate.m
@@ -106,19 +106,8 @@ NSString* const NotificationActionTwoIdent = @"ACTION_TWO";
     //设置接收消息代理
     [RCIM sharedRCIM].receiveMessageDelegate=self;
     
-    
-    //注册消息处理函数的处理方法,处理崩溃信息,写入本地
-    NSSetUncaughtExceptionHandler(&uncaughtExceptionHandler);
-    
-    
-    CrashManager *crashManager = [CrashManager defaultManager];
-    //Crash日志
-    if ([crashManager isCrashLog])
-    {
-        NSString *crashString = [crashManager crashLogContent];//Crash日志内容
-        LogRed(@"crashString = %@",crashString);//
-    }
-    [crashManager clearCrashLog];//清除Crash日志
+    // 将下面C函数的函数地址当做参数
+    NSSetUncaughtExceptionHandler(&UncaughtExceptionHandler);
     
     return YES;
 }
