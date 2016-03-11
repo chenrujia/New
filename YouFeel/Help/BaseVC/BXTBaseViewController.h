59af06519fa025bb053bbd4860750f3ad5fd0f2a
Jason318 <834699083@qq.com>
2016-03-11 15:45:39 +0800

    bsdbds

--- YouFeel/Help/BaseVC/BXTBaseViewController.h
+++ YouFeel/Help/BaseVC/BXTBaseViewController.h
@@ -33,6 +33,8 @@ typedef void (^HaveHidden)(BOOL hidden);
 
 - (void)hideMBP;
 
-- (NSString*)dataTOjsonString:(id)object;
+- (NSString*)dataToJsonString:(id)object;
+
+- (id)toArrayOrNSDictionary:(NSData *)jsonData;
 
 @end
